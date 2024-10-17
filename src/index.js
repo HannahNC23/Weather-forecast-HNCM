ec02960fdaa62575c74cbfa2e35cd0b2b4a2500f
HannahNC23 <hannah.charles@boots.co.uk>
2024-10-17 13:26:33 +0100

    Added search functionality using the API in JS

--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
-function handleSearchSubmit(event) {
-  event.preventDefault();
-  let searchInput = document.querySelector("#search-form-input");
-  let weatherCityElement = document.querySelector("#weather-city");
-  weatherCityElement.innerHTML = searchInput.value;
+function updateWeather(response) {
+  let currentTempValueElement = document.querySelector("#currentTempValue");
+  let temperature = response.data.temperature.current;
+  let weatherCityElement = document.querySelector("#weatherCity");
+  weatherCityElement.innerHTML = response.data.city;
+
+  currentTempValueElement.innerHTML = Math.round(temperature);
 }
 
-let searchFormElement = document.querySelector("#search-form");
+function searchCity(city) {
+  let apiKey = "et3cb33e62oc3445295794b051528aaf";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(updateWeather);
+}
+
+function handleSearchSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#searchFormInput");
+
+  searchCity(searchInput.value);
+}
+
+let searchFormElement = document.querySelector("#searchForm");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Nottingham");
